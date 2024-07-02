7f8996966a198475d49ed25441b47d4f38c1325f
igor1289 <igor1289@gmail.com>
2024-07-02 16:09:40 +0300

    14.3 (задание по фреймворку express)

---
+++ 14.3.js
@@ -0,0 +1,25 @@
+//Каркас API блога
+
+const {express, Router} = require("express");
+
+const app = express();
+
+//Функция-заглушка
+function dummy(req, res){}
+function auth_middleware_dummy(req, res, next){}
+
+//Маршрут до начальной страницы
+app.get('/', dummy)
+
+
+const postRouter = Router();
+postRouter.post('/new', auth_middleware_dummy, dummy)
+postRouter.get('/list/:author_id', dummy);
+postRouter.get('/show/:post_id', dummy);
+postRouter.post('/edit/:post_id', auth_middleware_dummy, dummy);
+postRouter.post('/delete/:post_id', auth_middleware_dummy, dummy);
+
+app.use('/post', postRouter);
+
+app.listend(8080);
+
